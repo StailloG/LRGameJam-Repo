3dd7ce71a1135bb05f2dd4058c3abc9d10a63048
StailloG <stephie.castles@gmail.com>
2022-06-14 22:32:04 -0400

    player movement

--- LongRelicUnity/Assets/Scripts/PlayerMovement.cs
+++ LongRelicUnity/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //variables
+    public Rigidbody2D playerRB;
+    public float speed = 6f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //call player rigidbody
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+
+        transform.position += movement * Time.deltaTime * speed;
     }
 }
