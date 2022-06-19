04ec4e300a0519e61c09a2d33d387874103cc786
StailloG <stephie.castles@gmail.com>
2022-06-18 23:56:22 -0400

    dialogue for black block

---
+++ LongRelicUnity/Assets/Scripts/GamePlayScripts/Dialogue Talk Scripts/BlackBlockDT.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BlackBlockDT : DialogueTalk
+{
+    //if person has extra dialogue, you would add them here 
+    [SerializeField] private Dialogue_Set intro;
+
+    public enum DialogueState { intro } //these are the potential dialogue states the character can be in, you can add more states if you need more dialogue
+    public DialogueState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = DialogueState.intro;//this is how you can change/set state. make sure at start it is set to intro
+                                    //in other scripts is where you would use "FindObjectOfType<BlockStopEmployeeDT>().state = DialogueState.action" to change states
+    }
+
+    public override void Talk()
+    {
+        //logic here to determine what to say 
+        //using enum to do that 
+        switch (state)
+        {
+            case DialogueState.intro:
+                IntroDialogue();   //this is from the inherited DialogueTalk Class 
+                StartCoroutine(NextLvl());
+                break;
+
+        }
+    }
+
+    public IEnumerator NextLvl()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(2);
+    }
+}
